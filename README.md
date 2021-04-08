0ea952ae8419c3fe3807d5a62bf3efede11e7d2c
Lauren Singbush <laurensingbush@Laurens-MacBook-Air.local>
2021-04-08 15:56:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# IMDb Favorites
+
+A React app with an Express backend that allows users to search the OMDb database for movies and shows. A successful search query will display up to 10 results in each category. Each result will have an option to open up a modal for additional information as well as the ability to add and remove the item from their favorites list.
+
+## Built with:
+
+- [Create React App](https://github.com/facebook/create-react-app)
+- [Node.js](https://nodejs.org/en/)
+- [Express](https://expressjs.com)
+- [Open Movie Database API](http://www.omdbapi.com/)
---
+++ server/server.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+const API_KEY = process.env.REACT_APP_OMDB_API_KEY;
+const PORT = process.env.PORT || 3080;
+const express = require('express');
+const cors = require('cors');
+const fetch = require('node-fetch');
+const path = require('path');
+const app = express();
+
+// middlewares
+app.use(express.urlencoded({ extended: true}));
+app.use(cors());
+
+// serve static files from React app
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+
+// respond with OMDb data by search query
+app.get('/api/search', async (req, res) => {
+    try {
+        const searchQuery = req.query.s;
+        if (searchQuery != null) {
+            const api = `http://www.omdbapi.com/?s=${searchQuery}&apikey=${API_KEY}`;
+            const response = await fetch(api, {headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }});
+            const responseJSON = await response.json();
+            res.json(responseJSON);
+        } else {
+            res.end();
+        } 
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+// respond with OMDb data by ID query
+app.get('/api/id', async (req, res) => {
+    try {
+        const id = req.query.i;
+        const api = `http://www.omdbapi.com/?i=${id}&apikey=${API_KEY}`;
+        const response = await fetch(api);
+        const responseJSON = await response.json();
+        res.json(responseJSON);
+    } catch(error) {
+        console.error(error);
+    }
+})
+
+// send back React's index.html file for requests that don't match above
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
+
